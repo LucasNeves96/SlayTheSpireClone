1b48b16ca3600d95a45738bfc3da35a7891dee6a
LucasNeves96 <lucasvalente.ng@gmail.com>
2025-07-31 21:52:33 -0300

    adding packages and card prefab

---
+++ ProjectSlaytheSpire/Assets/_Scripts/CardImage.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+
+public class CardImage : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer Image;
+    [SerializeField] private TMP_Text ManaValue;
+    [SerializeField] private TMP_Text Title;
+    [SerializeField] private TMP_Text Text;
+    [SerializeField] private GameObject Wrapper;
+}
